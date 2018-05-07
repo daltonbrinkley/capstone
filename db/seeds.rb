284e1e10a6c69ba7d70a618e35f454efbe82d255
daltonbrinkley <dbrinkles@gmail.com>
2018-05-07 16:16:17 -0500

    uncomment seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,53 +70,61 @@
 #   website: "http://5411empanadas.com/"})
 # location.save
 
-location = Location.new({
-  name: "Dollop Coffee Co.", 
-  address: "643 N Wells St, Chicago, IL 60654", 
-  hours: "7AM - 10PM", 
-  phone_number: "312-846-6670", 
-  website: "https://www.dollopcoffee.com/"})
-location.save
+# location = Location.new({
+#   name: "Dollop Coffee Co.", 
+#   address: "643 N Wells St, Chicago, IL 60654", 
+#   hours: "7AM - 10PM", 
+#   phone_number: "312-846-6670", 
+#   website: "https://www.dollopcoffee.com/"})
+# location.save
 
-location = Location.new({
-  name: "Café Integral at Freehand Chicago", 
-  address: "19 E Ohio St, Chicago, IL 60611", 
-  hours: "7AM - 5PM", 
-  phone_number: "312-940-3699", 
-  website: "https://freehandhotels.com/chicago/cafe-integral/"})
-location.save
+# location = Location.new({
+#   name: "Café Integral at Freehand Chicago", 
+#   address: "19 E Ohio St, Chicago, IL 60611", 
+#   hours: "7AM - 5PM", 
+#   phone_number: "312-940-3699", 
+#   website: "https://freehandhotels.com/chicago/cafe-integral/"})
+# location.save
 
-location = Location.new({
-  name: "Sawada Coffee", 
-  address: "112 N Green St, Chicago, IL 60607", 
-  hours: "8AM - 5PM", 
-  phone_number: "312-754-0431", 
-  website: "http://sawadacoffee.com/"})
-location.save
+# location = Location.new({
+#   name: "Sawada Coffee", 
+#   address: "112 N Green St, Chicago, IL 60607", 
+#   hours: "8AM - 5PM", 
+#   phone_number: "312-754-0431", 
+#   website: "http://sawadacoffee.com/"})
+# location.save
 
-location = Location.new({
-  name: "HERO Coffee Bar", 
-  address: "439 S Dearborn St, Chicago, IL 60605", 
-  hours: "7AM - 7PM", 
-  phone_number: "312-631-3269", 
-  website: "https://www.hero-coffee.com/"})
-location.save
+# location = Location.new({
+#   name: "HERO Coffee Bar", 
+#   address: "439 S Dearborn St, Chicago, IL 60605", 
+#   hours: "7AM - 7PM", 
+#   phone_number: "312-631-3269", 
+#   website: "https://www.hero-coffee.com/"})
+# location.save
 
-location = Location.new({
-  name: "The Wormhole Coffee", 
-  address: "1462 N Milwaukee Ave, Chicago, IL 60622", 
-  hours: "7AM - 9PM", 
-  phone_number: "773-661-2468", 
-  website: "http://www.thewormhole.us/"})
-location.save
+# location = Location.new({
+#   name: "The Wormhole Coffee", 
+#   address: "1462 N Milwaukee Ave, Chicago, IL 60622", 
+#   hours: "7AM - 9PM", 
+#   phone_number: "773-661-2468", 
+#   website: "http://www.thewormhole.us/"})
+# location.save
 
-location = Location.new({
-  name: "Lavazza Kafenio", 
-  address: "162 E Ohio St, Chicago, IL 60611", 
-  hours: "6AM - 6PM", 
-  phone_number: "312-255-8850", 
-  website: "http://www.kafeniochicago.com/"})
-location.save
+# location = Location.new({
+#   name: "Lavazza Kafenio", 
+#   address: "162 E Ohio St, Chicago, IL 60611", 
+#   hours: "6AM - 6PM", 
+#   phone_number: "312-255-8850", 
+#   website: "http://www.kafeniochicago.com/"})
+# location.save
+
+# location = Location.new({
+#   name: "Stan's Donuts - Diversey & Clark", 
+#   address: "2800 N Clark St, Chicago, IL 60657", 
+#   hours: "6AM - 9PM", 
+#   phone_number: "773-868-0173", 
+#   website: "http://www.stansdonutschicago.com/"})
+# location.save
 
 # review = Review.new({
 # title: "Busy corner Starbucks!", 
@@ -227,9 +235,52 @@ location.save
 #   })
 # image.save
 
-# image = Image.new({
-#   title: "Next Door Cafe", 
-#   url: "http://nelarusky.com/wp-content/uploads/2012/07/NextDoor.jpg", 
-#   location_id: 2
-#   })
-# image.save
+image = Image.new({
+  title: "Lavazza Kafenio", 
+  url: "https://www.garraways.co.uk/media/wysiwyg/garraways/categories/brands/lavazza/lavazza-coffee-at-garraways.jpg", 
+  location_id: 13
+  })
+image.save
+
+image = Image.new({
+  title: "The Wormhole Coffee", 
+  url: "http://www.thewormhole.us/wp-content/uploads/2013/11/DSC_0565-670x448.jpg", 
+  location_id: 12
+  })
+image.save
+
+image = Image.new({
+  title: "HERO Coffee Bar", 
+  url: "https://media.timeout.com/images/102540204/630/472/image.jpg", 
+  location_id: 11
+  })
+image.save
+
+image = Image.new({
+  title: "Sawada Coffee", 
+  url: "https://elatlboy.files.wordpress.com/2018/04/sawada-coffee-chicago-008.jpg", 
+  location_id: 10
+  })
+image.save
+
+image = Image.new({
+  title: "Café Integral at Freehand Chicago", 
+  url: "http://ccd.hwstatic.com/propertyimages/9/91255/79.jpg", 
+  location_id: 9
+  })
+image.save
+
+image = Image.new({
+  title: "Dollop Coffee Co.", 
+  url: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/f95a3c18466125.562c9ecc25eeb.jpg", 
+  location_id: 8
+  })
+image.save
+
+
+image = Image.new({
+  title: "Stan's Donuts", 
+  url: "https://static1.squarespace.com/static/59de6cc1bce1765a0a712259/5a04b8cae2c48354c3696f69/5a04c56f652deab388a855db/1510531988529/Stan%E2%80%99s+Donuts+%26+Coffee1.jpg?format=1500w", 
+  location_id: 5
+  })
+image.save
